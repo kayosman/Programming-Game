d1834440de7d6f55e21a2ec08f7b68568835b59b
idkayleasia <idk.ayeasia2002@gmail.com>
2023-03-08 14:17:20 -0500

    libs added, makefile

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	g++ -I src/include -L sc/lib -o *filename* -lmingw32 -lSDL2main -lSDL2
---
+++ include/window.h
@@ -0,0 +1,8 @@
+#include <stdio.h> /* printf and fprintf */
+
+#ifdef _WIN32
+#include <SDL2/SDL.h> /* Windows-specific SDL2 library */
+#else
+#include <SDL2/SDL.h> /* macOS- and GNU/Linux-specific */
+#endif
+
